0af47dd7db9d06acced2437a5a0f8fef664d8af8
Menezess42 <arielmenezess@hotmail.com>
2025-01-20 06:44:15 -0300

    evolving MezVim

--- mezVim/lua/m42/options.lua
+++ mezVim/lua/m42/options.lua
@@ -2,3 +2,46 @@ vim.o.relativenumber = true
 vim.o.number = true
 vim.o.termguicolors = true
 
+--- tabs and shits
+vim.opt.list=true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.o.smarttab=true
+vim.opt.cpoptions:append('I')
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.autoindent = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+-- stops line wrapping from being confusing
+vim.o.breakindent = true
+-- save undo history
+vim.o.undofile = true
+
+-- case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- set highlight on search
+vim.opt.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Derease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+-- vim.o.completeopt = 'menu, preview, noselect'
+
+-- minimal number of screen lines to keep above and below the cursor
+vim.opt.scrolloff = 27
+
+-- Enable mouse mode
+vim.o.mouse='a'
+
--- mezVim/lua/m42/remap.lua
+++ mezVim/lua/m42/remap.lua
@@ -8,14 +8,12 @@ vim.keymap.set("n", "nnp", vim.cmd.NoNeckPain)
 
 
 -- Ctrl+C Ctrl+V
-vim.keymap.set("v", "<leader>c", '"+y', { desc = "Copiar para o clipboard do sistema" })
-vim.keymap.set("n", "<leader>C", '"+yy', { desc = "Copiar linha para o clipboard do sistema" })
-vim.keymap.set("n", "<leader>v", '"+p', { desc = "Colar do clipboard do sistema depois do cursor" }) 
-vim.keymap.set("n", "<leader>V", '"+P', { desc = "Colar do clipboard do sistema antes do cursor" })
+vim.keymap.set({"n" , "v"}, "<leader>c", '"+y', { desc = "Copiar para o clipboard do sistema" })
+vim.keymap.set({"n","v"}, "<leader>C", '"+yy', { desc = "Copiar linha para o clipboard do sistema" })
+vim.keymap.set({"n","v"}, "<leader>v", '"+p', { desc = "Colar do clipboard do sistema depois do cursor" }) 
+vim.keymap.set({"n","v"}, "<leader>V", '"+P', { desc = "Colar do clipboard do sistema antes do cursor" })
+--
+--
 
-
--- keep paste buffer
-vim.keymap.set("v", "p", '"_dP', { desc = "Colar sem alterar o buffer de colagem" })
-
-
+vim.keymap.set("n", "<leader>q", ":Bdelete<CR>")
 
