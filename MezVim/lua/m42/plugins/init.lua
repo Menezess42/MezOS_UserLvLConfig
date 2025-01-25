44de66e849ecf79f5100ec6f732dcbc0be2572cf
Menezess42 <arielmenezess@hotmail.com>
2025-01-25 16:46:51 -0300

    config noNeckpain and reconfig oil

--- MezVim/lua/m42/plugins/init.lua
+++ MezVim/lua/m42/plugins/init.lua
@@ -1,7 +1,9 @@
+require('m42.plugins.oilvim')
+require('m42.plugins.nnpain')
 require('lze').load{
     {import = "m42.plugins.telescope", },
     {import = "m42.plugins.treesitter", },
     {import = "m42.plugins.completion", },
     {import = "m42.plugins.autopairs", },
-    {import = "m42.plugins.oilvim", },
+    -- {import = "m42.plugins.oilvim", },
 }
