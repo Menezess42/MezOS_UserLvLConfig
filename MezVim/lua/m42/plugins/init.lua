3b91c6f5f43d836ad93a93ebe39ff2e29c8a7439
Menezess42 <arielmenezess@hotmail.com>
2025-01-25 17:26:06 -0300

    test

--- MezVim/lua/m42/plugins/init.lua
+++ MezVim/lua/m42/plugins/init.lua
@@ -9,5 +9,13 @@ require('lze').load{
     {import = "m42.plugins.markdownprev"},
     {import = "m42.plugins.undotree"},
     {import = "m42.plugins.comment"},
-    {import = "m42.plugins.indentblankline"},
+    -- {import = "m42.plugins.indentblankline"},
+    {
+        'ident-blankline.nvim',
+        for_cat = 'general.extra',
+        event = 'DeferredUIEnter',
+        after = function (plugin)
+            require('ibl').setup()
+        end
+    },
 }
