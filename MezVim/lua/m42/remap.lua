d645c569cfef94c23a18ab395a59216b959cbc78
Menezess42 <arielmenezess@hotmail.com>
2025-01-25 19:35:39 -0300

    finished doing my nvim based config

--- MezVim/lua/m42/remap.lua
+++ MezVim/lua/m42/remap.lua
@@ -1,27 +1,34 @@
 vim.g.mapleader = ' '
 
+-- No neck pian
 vim.keymap.set("n", "nnp", vim.cmd.NoNeckPain)
 
+-- Ctrl+c v for nvim to work with clipboard
 vim.keymap.set({"n", "v"}, "<leader>c", '"+y', {desc = "Copiar para o clipboard do sistema"})
 vim.keymap.set({"n", "v"}, "<leader>C", '"+yy', {desc = "Copiar linha para o clipboard do sistema"})
 vim.keymap.set({"n","v"}, "<leader>v", '"+p', {desc = "Colar do clipboard do sistema depois do cursor"})
 vim.keymap.set({"n","v"},"<leader>V", '"+P',{desc = "coloar do clipboard do sistema antes do cursor"})
 
-vim.keymap.set("n", "<leader>q", ":Bdelete<CR>")
-
+-- Keymaps for better default experience
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {desc = 'Moves line Down'})
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {desc = 'Moves Line Up'})
-
 vim.keymap.set("n", "<C-d>", "<C-d>zz", {desc = 'Scroll Down'})
-vim.keymap.set("n", "<C-d>", "<C-u>zz", {desc = 'Scroll Up'})
+vim.keymap.set("n", "<C-u>", "<C-u>zz", {desc = 'Scroll Up'})
 vim.keymap.set("n", "n", "nzzzv", {desc = 'Next Search Result'})
 vim.keymap.set("n", "N", "Nzzzv", {desc = 'Previous Search Result'})
 
+vim.keymap.set("n", "<leader><leader>h", "<cmd>bprev<CR>", { desc = 'Previous buffer' })
+vim.keymap.set("n", "<leader><leader>l", "<cmd>bnext<CR>", { desc = 'Next buffer' })
+vim.keymap.set("n", "<leader><leader>]", "<cmd>b#<CR>", { desc = 'Last buffer' })
+vim.keymap.set("n", "<leader><leader>d", "<cmd>bdelete<CR>", { desc = 'delete buffer' })
+
+-- dealing with some misstyping behavior
 vim.cmd([[command! W w]])
 vim.cmd([[command! Wq wq]])
 vim.cmd([[command! WQ wq]])
 vim.cmd([[command! Q q]])
 
+-- remap for dealing with word wrap 
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", {expr = true, silent = true})
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", {expr = true, silent = true})
 
