c3e79bec59c59b2d901c40e0d3db0629f30f545b
Menezess42 <arielmenezess@hotmail.com>
2025-01-27 12:02:49 -0300

    almost there

--- MezVim/lua/m42/plugins/init.lua
+++ MezVim/lua/m42/plugins/init.lua
@@ -4,20 +4,19 @@ require('m42.plugins.highlight')
 require('m42.plugins.rainbowDelimiter')
 
 require('lze').load{
+    {import = "m42.plugins.aerial", },
     {import = "m42.plugins.telescope", },
     {import = "m42.plugins.treesitter", },
     {import = "m42.plugins.completion", },
     {import = "m42.plugins.autopairs", },
-    {import = "m42.plugins.lazydev"},
-    {import = "m42.plugins.markdownprev"},
-    {import = "m42.plugins.undotree"},
-    {import = "m42.plugins.comment"},
-    {import = "m42.plugins.indentblankline"},
-    {import = "m42.plugins.vimSurround"},
-    {import = "m42.plugins.fidget"},
-    {import = "m42.plugins.lualine"},
-    {import = "m42.plugins.gitsigns"},
-    {import = "m42.plugins.wichKey"},
-    -- {import = "m42.plugins.pythonConf"}
-    {import = "m42.plugins.aerial"},
+    {import = "m42.plugins.lazydev",},
+    {import = "m42.plugins.markdownprev",},
+    {import = "m42.plugins.undotree",},
+    {import = "m42.plugins.comment",},
+    {import = "m42.plugins.indentblankline",},
+    {import = "m42.plugins.vimSurround",},
+    {import = "m42.plugins.fidget",},
+    {import = "m42.plugins.lualine",},
+    {import = "m42.plugins.gitsigns",},
+    {import = "m42.plugins.wichKey",},
 }
