643bdd1d40bad10d9b523dbf56baf35c558d5b62
Menezess42 <arielmenezess@hotmail.com>
2025-01-27 12:05:31 -0300

    almost there

--- MezVim/lua/m42/plugins/aerial.lua
+++ MezVim/lua/m42/plugins/aerial.lua
@@ -1,5 +1,16 @@
 return {
     {
         'aerial',
-    }
+        after = function(plugin)
+            require("aerial").setup({
+                -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+                on_attach = function(bufnr)
+                    -- Jump forwards/backwards with '{' and '}'
+                    vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+                    vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+                end,
+            })
+        end,
+        vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+    },
 }
