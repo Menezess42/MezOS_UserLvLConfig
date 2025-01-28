023867894fbd2d71e14511631db1f46e4f0101b1
Menezess42 <arielmenezess@hotmail.com>
2025-01-28 11:55:41 -0300

    empity commit comment to fast work

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -7,8 +7,20 @@
   config = lib.mkIf config.tmux.enable {
     programs.tmux = {
       enable = true; # Habilita o Tmux no sistema
-      defaultTerminal = "screen-256color"; # Define o terminal padrão para suportar 256 cores
       extraConfig = ''
+        # Change leader key
+      set -g prefix C-s 
+
+        # Activate mouse
+      set -g mouse on
+
+        # Change moviment from arrowkeys to vim motions
+      bind-key h select-pane -L
+      bind-key j select-pane -D
+      bind-key k select-pane -U
+      bind-key l select-pane -R
+
+      set-option -g status-position top 
       '';
     };
   };
