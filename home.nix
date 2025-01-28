84e997b3640d77a03adab913b4de5a9975f0329f
Menezess42 <arielmenezess@hotmail.com>
2025-01-28 11:46:39 -0300

    empity commit comment to fast work

--- home.nix
+++ home.nix
@@ -9,7 +9,7 @@
     ./modules/tmux.nix
   ];
   zsh.enable=true;
-  tmux.enable=false;
+  tmux.enable=true;
   home.packages = with pkgs; [
   # neovim
   xclip
@@ -42,7 +42,6 @@
     qalculate-gtk
     git
     thefuck
-    tmux
   ];
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
