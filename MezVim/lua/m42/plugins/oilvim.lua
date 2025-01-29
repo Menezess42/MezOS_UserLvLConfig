1940a4333019801c092d4343c07643c076a920b4
Menezess42 <arielmenezess@hotmail.com>
2025-01-28 22:08:31 -0300

    new keybinds

--- MezVim/lua/m42/plugins/oilvim.lua
+++ MezVim/lua/m42/plugins/oilvim.lua
@@ -10,6 +10,12 @@ if nixCats('general.extra') then
         keymaps = {
             ["g?"] = "actions.show_help",
             ["<CR>"] = "actions.select",
+            ["<C-s>"] = "actions.select_vsplit",
+            -- ["<C-h>"] = "actions.select_split",
+            ["<C-t>"] = "actions.select_tab",
+            ["<C-p>"] = "actions.preview",
+            ["<C-c>"] = "actions.close",
+            ["<C-;>"] = "actions.refresh",
             ["-"] = "actions.parent",
             ["_"] = "actions.open_cwd",
             ["`"] = "actions.cd",
