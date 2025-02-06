c1dfba19f1e298866e818d4780cc4381047c86c7
Menezess42 <arielmenezess@hotmail.com>
2025-02-06 12:33:28 -0300

    sql in vim

--- MezVim/lua/m42/plugins/init.lua
+++ MezVim/lua/m42/plugins/init.lua
@@ -52,6 +52,8 @@ require('lze').load{
         init = function()
             -- Your DBUI configuration
             vim.g.db_ui_use_nerd_fonts = 1
+            vim.g.db_ui_env_variable_url = 'DATABASE_URL'
+            vim.g.db_ui_env_variable_name = 'DATABASE_NAME'
         end,
     }
 }
