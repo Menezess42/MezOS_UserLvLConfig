a3e549add552651623cc046313176b8bbfa69cd5
Menezess42 <arielmenezess@hotmail.com>
2025-07-05 20:31:46 -0300

    Lets chekcout molten

--- MezVim/lua/m42/plugins/init.lua
+++ MezVim/lua/m42/plugins/init.lua
@@ -2,6 +2,7 @@ require('m42.plugins.nnpain')
 require('m42.plugins.oilvim')
 require('m42.plugins.highlight')
 require('m42.plugins.rainbowDelimiter')
+require('m42.plugins.molten')
 
 require('lze').load{
     {import = "m42.plugins.telescope", },
