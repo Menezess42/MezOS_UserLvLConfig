ebce47e948103bb089ea3cd7d79fd954f1795e84
Menezess42 <arielmenezess@hotmail.com>
2025-07-05 19:11:47 -0300

    Lets chekcout molten

--- MezVim/lua/m42/plugins/ufo.lua
+++ MezVim/lua/m42/plugins/ufo.lua
@@ -1,29 +1,29 @@
 return {
   {
     "kevinhwang91/nvim-ufo",
-    requires = "kevinhwang91/promise-async",  -- dependência para funcionamento assíncrono
+    requires = "kevinhwang91/promise-async",  -- Dependência para funcionamento assíncrono
     for_cat = 'general.extra',
-    event = "deferreduienter",  -- carrega após a iu ser inicializada
+    event = "DeferredUIEnter",  -- Carrega após a IU ser inicializada
     after = function(plugin)
-      -- configurações de dobra
+      -- Configurações de dobra
       vim.o.foldcolumn = '1'
       vim.o.foldlevel = 99
       vim.o.foldenable = true
 
-      -- mapeamentos de teclas para manipulação de dobras
-      vim.keymap.set('n', 'zr', require('ufo').openallfolds, { desc = "abrir todas as dobras" })
-      vim.keymap.set('n', 'zm', require('ufo').closeallfolds, { desc = "fechar todas as dobras" })
-      vim.keymap.set('n', 'zk', function()
-        -- se não houver janela de visualização, utiliza o hover do lsp
+      -- Mapeamentos de teclas para manipulação de dobras
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = "Abrir todas as dobras" })
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = "Fechar todas as dobras" })
+      vim.keymap.set('n', 'zK', function()
+        -- Se não houver janela de visualização, utiliza o hover do LSP
         if vim.fn.win_getid() == 0 then
           vim.lsp.buf.hover()
         end
-      end, { desc = "visualizar dobra" })
+      end, { desc = "Visualizar dobra" })
 
-      -- inicialização do ufo
+      -- Inicialização do ufo
       require('ufo').setup({
         provider_selector = function(bufnr, filetype, buftype)
-          -- prioriza lsp e indentação como provedores de dobra
+          -- Prioriza LSP e indentação como provedores de dobra
           return { 'lsp', 'indent' }
         end,
       })
