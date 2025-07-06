171020a106181c14c44f9b834ebe9d16fd347419
Menezess42 <arielmenezess@hotmail.com>
2025-07-05 22:54:43 -0300

    let's try molten round 2

--- MezVim/lua/m42/plugins/molten.lua
+++ MezVim/lua/m42/plugins/molten.lua
@@ -1,12 +1,28 @@
+-- File: lua/m42/plugins/molten.lua
+-- Integração do plugin molten-nvim em m42
 return {
   {
     "benlubas/molten-nvim",
-    for_cat = "general.always",    -- ou deixe sem categoria
-    event = "VimEnter",            -- carrega assim que abrir o nvim
-    after = function()
-      vim.g.molten_image_provider = "image.nvim"
-      -- suas outras configurações…
-      print("✅ Molten carregado!")
+    requires = {
+      "samodostal/image.nvim",  -- plugin de renderização de imagens
+      "nvim-lua/plenary.nvim"  -- utilitários Lua para Neovim
+    },
+    for_cat = 'general.molten',  -- categoria definida no flake
+    ft = { "markdown", "jupyter" },  -- carrega em arquivos Markdown e Jupyter
+    cmd = { "MoltenStart", "MoltenStop" },  -- comandos do plugin
+    event = "BufReadPost",  -- carregamento pós leitura de buffer
+    config = function()
+      require('molten').setup({
+        -- Configurações opcionais, consulte a documentação para mais opções
+        image = {
+          backend = "imagemagick",  -- usa imagemagick instalado via Nix
+        },
+        -- pinte as células de Jupyter como imagens
+        display = {
+          syntax = true,
+          prompt = false,
+        },
+      })
     end,
-  }
+  },
 }
