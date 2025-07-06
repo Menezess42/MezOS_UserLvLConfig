9f6d563947f697f0fa104e11b18303644844f743
Menezess42 <arielmenezess@hotmail.com>
2025-07-06 00:19:25 -0300

    flake updata

--- MezVim/lua/m42/plugins/molten.lua
+++ MezVim/lua/m42/plugins/molten.lua
@@ -1,47 +1,47 @@
-return {
-  {
-    "benlubas/molten-nvim",
-    requires = {
-      "samodostal/image.nvim",
-      "nvim-lua/plenary.nvim"
-    },
-    -- for_cat = "general.molten",
-    ft = { "markdown", "jupyter" },
-    cmd = { "MoltenInit", "MoltenEvaluateOperator", "MoltenEvaluateVisual", "MoltenEvaluateLine" },
-    event = "DeferredUIEnter",
-    after = function()
-      local ok, molten = pcall(require, "molten")
-      if not ok then
-        vim.notify("Molten plugin não pôde ser carregado", vim.log.levels.ERROR)
-        return
-      end
-
-      molten.setup({
-        image = {
-          backend = "imagemagick",
-        },
-        display = {
-          syntax = true,
-          prompt = false,
-        },
-      })
-    end,
-  },
-}
 -- return {
 --   {
 --     "benlubas/molten-nvim",
---     dependencies = {
---       "3rd/image.nvim"
+--     requires = {
+--       "samodostal/image.nvim",
+--       "nvim-lua/plenary.nvim"
 --     },
---     for_cat = "general.molten",
---     ft = { "python", "ipynb", "markdown" },
---     build = ":UpdateRemotePlugins",
---     after = function(plugins)
---       require("molten").setup({})
+--     -- for_cat = "general.molten",
+--     ft = { "markdown", "jupyter" },
+--     cmd = { "MoltenInit", "MoltenEvaluateOperator", "MoltenEvaluateVisual", "MoltenEvaluateLine" },
+--     event = "DeferredUIEnter",
+--     after = function()
+--       local ok, molten = pcall(require, "molten")
+--       if not ok then
+--         vim.notify("Molten plugin não pôde ser carregado", vim.log.levels.ERROR)
+--         return
+--       end
+--
+--       molten.setup({
+--         image = {
+--           backend = "imagemagick",
+--         },
+--         display = {
+--           syntax = true,
+--           prompt = false,
+--         },
+--       })
 --     end,
---   }
+--   },
 -- }
+return {
+  {
+    "benlubas/molten-nvim",
+    dependencies = {
+      "3rd/image.nvim"
+    },
+    for_cat = "general.molten",
+    ft = { "python", "ipynb", "markdown" },
+    build = ":UpdateRemotePlugins",
+    after = function(plugins)
+      require("molten").setup({})
+    end,
+  }
+}
 -- File: lua/m42/plugins/molten.lua
 -- Integração do plugin molten-nvim em m42
 -- return {
